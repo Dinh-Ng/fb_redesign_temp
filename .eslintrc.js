76186d8e52c0a83b9a200dce06d228d0f71b3dad
DiNg <dinhquangnguyen1997@gmail.com>
2023-02-13 00:10:00 +0700

    Eslint: module-resolver and import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,34 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        'sort-imports': [
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'simple-import-sort/imports': [
           'error',
           {
-            ignoreCase: false,
-            ignoreDeclarationSort: false,
-            ignoreMemberSort: false,
-            memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-            allowSeparatedGroups: false,
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
+            ],
           },
         ],
+        'simple-import-sort/exports': 'error',
       },
     },
   ],
