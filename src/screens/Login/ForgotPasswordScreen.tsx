2d518dbe930acb47833329c5d664aab9c04d2372
DiNg <dinhquangnguyen1997@gmail.com>
2023-03-07 23:58:33 +0700

    ForgotPassword Screen: ---

--- src/screens/Login/ForgotPasswordScreen.tsx
+++ src/screens/Login/ForgotPasswordScreen.tsx
@@ -3,15 +3,39 @@ import styled from 'styled-components/native';
 
 import {ScreenWrapper} from '@/components/ScreenWrapper';
 import RegisterHeader from '@/screens/Login/components/RegisterHeader';
+import {Colors} from '@/themes/Colors';
 
 const ForgotPasswordScreen = () => {
   return (
-    <Wrapper>
+    <Container>
       <RegisterHeader title={'Find Your Account'} />
-    </Wrapper>
+      <Wrapper>
+        <StepTitle>Enter your phone number</StepTitle>
+        <StepDescription>Enter your phone number</StepDescription>
+      </Wrapper>
+    </Container>
   );
 };
 
-const Wrapper = styled(ScreenWrapper)``;
+const Container = styled(ScreenWrapper)``;
+
+const Wrapper = styled(ScreenWrapper)`
+  align-items: center;
+`;
+
+const StepTitle = styled.Text`
+  font-weight: 600;
+  font-size: 18px;
+  color: ${Colors.black};
+  margin: 40px 0 20px 0;
+`;
+
+const StepDescription = styled.Text`
+  font-weight: 400;
+  font-size: 12px;
+  color: ${Colors.gray1};
+  text-align: center;
+  margin-bottom: 50px;
+`;
 
 export default memo(ForgotPasswordScreen);
