3140ce06446ca8ca9c9e9272665dca96524d77dd
DiNg <dinhquangnguyen1997@gmail.com>
2023-03-11 01:02:00 +0700

    Resource: Add icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,8 @@ export const IC_SEARCH = require('./icons/Search.png');
 export const IC_BACK = require('./icons/Back.png');
 export const IC_UNCHECK_RADIO = require('./icons/ic_uncheck_radio.png');
 export const IC_CHECK_RADIO = require('./icons/ic_check_radio.png');
+export const IC_EMAIL = require('./icons/ic_email.png');
+export const IC_SMS = require('./icons/ic_sms.png');
 
 export const IMG_LOGO = require('./images/Logo.png');
 export const IMG_AVATAR = require('./images/Profile.png');
