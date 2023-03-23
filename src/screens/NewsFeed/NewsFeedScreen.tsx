633f343f95b7812cf9e411e0a7c1963bf3e85043
DiNg <dinhquangnguyen1997@gmail.com>
2023-03-23 23:59:31 +0700

    NewFeed Screen

--- src/screens/NewsFeed/NewsFeedScreen.tsx
+++ src/screens/NewsFeed/NewsFeedScreen.tsx
@@ -1,7 +1,34 @@
 import React, {memo} from 'react';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import styled from 'styled-components/native';
+
+import {IMG_AVATAR} from '@/assets';
+import {Colors} from '@/themes/Colors';
 
 const NewsFeedScreen = () => {
-  return <></>;
+  return (
+    <Container>
+      <CreatePostContainer>
+        <CreatePostAvatar source={IMG_AVATAR} />
+      </CreatePostContainer>
+    </Container>
+  );
 };
+
+const Container = styled(KeyboardAwareScrollView)`
+  padding-top: 10px;
+  background-color: ${Colors.white};
+`;
+
+const CreatePostContainer = styled.View`
+  flex-direction: row;
+`;
+
+const CreatePostAvatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+  margin: 0 20px;
+`;
 
 export default memo(NewsFeedScreen);
